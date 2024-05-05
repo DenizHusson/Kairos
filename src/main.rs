6726a0d136eaed790392a559c50ca0b654097ce1
Deniz Uzuntok Husson <deniz.uzuntok-husson@epitech.eu>
2024-05-06 00:53:12 +0200

    feat(inputs): get user inputs
    Issue #3 get the users input.
    
    I call in 'main()' the new implemented function 'kairos_get_input()' that return a vector
    with the user inputs.
    
    First you get the project name which will be the project repository and the project binary
    name, after you get the 'HTTPS Github URL', the string is a '\n' if the user don't want to
    clone a project. Finally we get the coding language, you can have two anser for each
    language, the name or the index.
    
    We push all the inputs in a vector and we return it.
    
    Deniz Uzuntok Husson
    deniz.uzuntok-husson@epitech.eu

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,27 @@
 // File: main.rs
 // ---
 
+use std::io;
+
+fn kairos_get_input() -> Vec<String> {
+    let mut inputs: Vec<String> = Vec::new();
+    let mut project_name = String::new();
+    let mut github_https = String::new();
+    let mut code_language = String::new();
+
+    println!("Project name:");
+    io::stdin().read_line(&mut project_name).expect("Failed to read the line");
+    println!("Github HTTPS URL (empty to skip):");
+    io::stdin().read_line(&mut github_https).expect("Failed to read the line");
+    println!("Language: 1.C   2.C++   3.Python   4.Haskell   5.Rust");
+    io::stdin().read_line(&mut code_language).expect("Failed to read the line");
+
+    inputs.push(project_name);
+    inputs.push(github_https);
+    inputs.push(code_language);
+    inputs
+}
+
 fn main() {
-    println!("Hello on Kairos repository");
+    let _inputs = kairos_get_input();
 }
